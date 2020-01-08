f2310e70989ce6f92bedba0d882ada605b15e23a
Amit Kumar <akamit21@outlook.com>
2020-01-08 12:20:40 +0530

    Modified Login page added login form

--- src/client/src/routes/Login.jsx
+++ src/client/src/routes/Login.jsx
@@ -5,6 +5,17 @@ const Login = () => {
   return (
     <>
       <div>LOGIN PAGE</div>
+      <form>
+        <div>
+          <input type="email" placeholder="Email" />
+        </div>
+        <div>
+          <input type="password" placeholder="Password" />
+        </div>
+        <div>
+          <input type="submit" value="Log in" />
+        </div>
+      </form>
       <Link to="/dash">GOTO DASHBOARD</Link>
     </>
   );
